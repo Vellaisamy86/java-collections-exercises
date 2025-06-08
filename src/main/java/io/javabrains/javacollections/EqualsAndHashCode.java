27183ac84f34c6194d36441766982dccda91bf52
Vellaisamy86 <66508392+Vellaisamy86@users.noreply.github.com>
2025-06-08 22:09:58 +0800

    Initial committ

--- src/main/java/io/javabrains/javacollections/EqualsAndHashCode.java
+++ src/main/java/io/javabrains/javacollections/EqualsAndHashCode.java
@@ -6,9 +6,59 @@ Generate equals and hashcode methods using member variables firstName, lastName,
 Compare two instances of Person that have the same data
 */
 
+import java.util.Date;
+import java.util.Objects;
+
 public class EqualsAndHashCode {
 
     public static void main(String[] args) {
+        Person person1 = new Person("John", "Doe", 30, new Date());
+        Person person2 = new Person("John", "Doe", 30, new Date());
 
+        // Compare two instances of Person
+        if (person1.equals(person2)) {
+            System.out.println("person1 is equal to person2");
+        } else {
+            System.out.println("person1 is not equal to person2");
+        }
+
+        // Print hash codes
+        System.out.println("Hash code of person1: " + person1.hashCode());
+        System.out.println("Hash code of person2: " + person2.hashCode());
+
+    }
+}
+
+class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Date  lastModifiedDate;
+
+    public Person(String firstName, String lastName, int age, Date lastModifiedDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        if (!(obj instanceof Person)) return false;
+        Person person = (Person) obj;
+        return this.age == person.age
+                && Objects.equals(this.firstName, person.firstName)
+                && Objects.equals(this.lastName, person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
+           /* int result = firstName.hashCode();
+            result = 31 * result + lastName.hashCode();
+            result = 31 * result + age;
+            return result;*/
     }
 }
--- src/main/java/io/javabrains/javacollections/IteratorExercise.java
+++ src/main/java/io/javabrains/javacollections/IteratorExercise.java
@@ -2,6 +2,8 @@ package io.javabrains.javacollections;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
 
 /*
 Write code that calls the getFruits method and then iterates over the collection, printing each element to console
@@ -9,6 +11,27 @@ Write code that calls the getFruits method and then iterates over the collection
 
 public class IteratorExercise {
 
+    public static void main(String[] args) {
+        FruitSupplier fruitSupplier = new FruitSupplier();
+        Collection<String> fruits = fruitSupplier.getFruits();
+        // Iterate over the collection and print each fruit
+
+        // Using Iterator to iterate over the collection
+        System.out.println("Print Fruits using Iterator pattern start time:"+ new Date().getTime());
+        Iterator<String> iterator = fruits.iterator();
+        while (iterator.hasNext()) {
+            String fruit = iterator.next();
+            System.out.println("Fruit String=  "+fruit);
+        }
+        System.out.println("Print Fruits using Iterator pattern end time:"+ new Date().getTime());
+
+        // Using enhanced for loop to iterate over the collection
+        System.out.println("Using enhanced for loop to iterate over the collection start time:"+ new Date().getTime());
+        for (String fruit : fruits) {
+            System.out.println("Fruit String=  "+fruit);
+        }
+        System.out.println("Using enhanced for loop to iterate over the collection end time:"+ new Date().getTime());
+    }
 
 }
 
